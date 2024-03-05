b608059b3c8bcf61f6fc7dce485eb26e2bb8c2fb
csortansz <csortansz@innomatrix.hu>
2024-03-06 00:12:42 +0100

    refactor simplify game query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,35 +1,35 @@
-import {GameQuery} from "../App";
-import ApiClient, {FetchResponse} from "../services/api-client";
-import {Platform} from "./usePlatforms";
-import {useInfiniteQuery} from "@tanstack/react-query";
+import { GameQuery } from "../App";
+import ApiClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const apiClient = new ApiClient<Game>("/games");
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-    rating_top: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
-    useInfiniteQuery<FetchResponse<Game>, Error>({
-        queryKey: ["games", gameQuery],
-        queryFn: ({pageParam = 1}) =>
-            apiClient.getAll({
-                params: {
-                    genres: gameQuery.genre?.id,
-                    parent_platforms: gameQuery.platform?.id,
-                    ordering: gameQuery.sortOrder,
-                    search: gameQuery.searchText,
-                    page: pageParam
-                },
-            }),
-        getNextPageParam: (lastPage, allPages) => {
-            return lastPage.next ? allPages.length + 1 : undefined;
-        }
-    });
+  useInfiniteQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam,
+        },
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+  });
 
 export default useGames;
