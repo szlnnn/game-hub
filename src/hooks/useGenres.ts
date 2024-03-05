16fdc6c6b8011df1280d55e4043ed11260527ccb
csortansz <csortansz@innomatrix.hu>
2024-03-06 00:23:18 +0100

    using ms for time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,19 +1,20 @@
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new ApiClient<Genre>("/genres");
 
 export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
+  id: number;
+  name: string;
+  image_background: string;
 }
 
 const useGenres = () =>
-    useQuery({
-        queryKey: ["genres"],
-        queryFn: apiClient.getAll,
-        staleTime: 24 * 60 * 60 * 1000,
-    });
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: apiClient.getAll,
+    staleTime: ms("24h"),
+  });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,19 +1,20 @@
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new ApiClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 const usePlatforms = () =>
-    useQuery({
-        queryKey: ["platforms"],
-        queryFn: apiClient.getAll,
-        staleTime: 24 * 60 * 60 * 1000,
-    });
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: apiClient.getAll,
+    staleTime: ms("24h"),
+  });
 
 export default usePlatforms;
